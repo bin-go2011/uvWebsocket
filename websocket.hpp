b1f97bb27b5967c709230c4f6e00b8b7e30a476f
abc <a@b.c>
2020-07-10 08:39:47 +0800

    divide into two parts

--- websocket.hpp
+++ websocket.hpp
@@ -80,26 +80,5 @@ public:
     WebSocketHeaders custom_headers;
 };
 
-class WebSocketClient : public WebSocket {
-private:
-    Url::Query query_params;
-    std::string path, host, port;
-
-    static void on_getaddrinfo_end(uv_getaddrinfo_t* req, int status,
-                addrinfo* res);
-    static void on_connect_end(uv_connect_t* req, int status);
-    static void on_alloc_callback(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf);
-    static void on_read_callback(uv_stream_t* client, ssize_t nbuf, const uv_buf_t* buf);
-
-    void on_tcp_connect();
-    void on_tcp_packet(char* packet, ssize_t len);
-    void on_tcp_close(int status);
-
-public:
-    WebSocketClient(uv_loop_t* loop, uv_tcp_t* socket = nullptr);
-
-    void connect(std::string uri);
-    void connect(addrinfo* addr);
-};
 
 #endif
