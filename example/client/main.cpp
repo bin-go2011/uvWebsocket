4f900af3752ddd375913d5bd346994895ad38f08
abc <a@b.c>
2020-07-15 15:14:42 +0800

    use nlohmann::json

--- example/client/main.cpp
+++ example/client/main.cpp
@@ -1,4 +1,9 @@
-#include "websocket.hpp"
+#include <iostream>
+#include "websocket_client.hpp"
+#include "json.hpp"
+
+// for convenience
+using json = nlohmann::json;
 
 int main() {
 
@@ -6,7 +11,14 @@ int main() {
 
     WebSocketClient ws(loop);
 
-    ws.connect("ws://localhost:1501");
+    ws.on_message = [](WebSocket* ws, char* data, size_t len, WebSocketOpcode opcode) {
+        auto j = json::parse(std::string(data, len));
+        std::cout << j.dump() << std::endl;
+    };
+
+    ws.connect("ws://10.220.214.47:8089/ipc/ws/1.0/relay");
+
+    uv_run(loop, UV_RUN_DEFAULT);
 
     return 0;
 }
